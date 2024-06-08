00528db284efc762bb303f5ded16ee67ff46dbb5
sebalooc35 <andres.ordonez@galileo.edu>
2024-06-08 12:29:32 -0600

    add security to project

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,7 @@
+package org.ada.school.utils;
+
+public class Constants {
+    public static String CLAIMS_ROLES_KEY = "roles";
+    public static String COOKIE_NAME = "jwt_token";
+    public static int TOKEN_DURATION_MINUTES = 30;
+}
